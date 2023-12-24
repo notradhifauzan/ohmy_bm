9e090b467f9854630a3255ed8a4866f1d372ef4c
Irfanizam <irfanizam34@gmail.com>
2023-12-24 21:47:53 +0800

    test gitignore

--- app/config/config.php
+++ app/config/config.php
@@ -3,14 +3,14 @@
     //DB params
     define('DB_HOST','localhost');
     define('DB_USER','root');
-    define('DB_PASS','');
+    define('DB_PASS','Security@12');
     define('DB_NAME','portal_bahasa_melayu');
 
     // App root
     define('APPROOT',dirname(dirname(__FILE__))) . '<br>';
 
     //URL Root
-    define('URLROOT','http://localhost/ohmy_bm');
+    define('URLROOT','http://localhost:8001/ohmy_bm');
 
     //Site name
     define('SITENAME','Oh My Bm!');
